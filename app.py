50eb7c34cb229589b6f55b4d2be2c9211cfe44bd
Manoj25 <manoj26hyd@gmail.com>
2021-06-04 08:38:37 +0530

    Changed app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+class Student:
+    
+    def __init__(self, m1, m2, m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+
+    def display(self):
+        print("M1 Marks:", self.m1)
+        print("M2 Marks:", self.m2)
+        print("M3 Marks:", self.m3)    
